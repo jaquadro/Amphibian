38bebf31c5158943fa0c7c155a8d522c86577146
Justin <Justin@Tallon.hsd1.ma.comcast.net.>
2012-07-03 02:10:17 -0400

    Drawing updates, BackdropSpatial (evolution of Parallax, compatible with RenderSystem)

--- Amphibian.Drawing/DrawBatch.cs
+++ Amphibian.Drawing/DrawBatch.cs
@@ -30,6 +30,8 @@ namespace Amphibian.Drawing
         private int _indexBufferIndex;
         private int _vertexBufferIndex;
 
+        private Triangulator _triangulator;
+
         private static BasicEffect _effect;
 
         public DrawBatch (GraphicsDevice device)
@@ -183,7 +185,6 @@ namespace Amphibian.Drawing
                 throw new InvalidOperationException();
 
             RequestBufferSpace(4, 6);
-
             AddInfo(4, 6, brush);
 
             int baseVertexIndex = _vertexBufferIndex;
@@ -197,11 +198,35 @@ namespace Amphibian.Drawing
             AddTriangle(baseVertexIndex + 1, baseVertexIndex + 3, baseVertexIndex + 2);
         }
 
+        public void FillPath (IList<Vector2> points, int offset, int count, Brush brush)
+        {
+            if (_triangulator == null)
+                _triangulator = new Triangulator();
+
+            _triangulator.Triangulate(points, offset, count);
+
+            RequestBufferSpace(count, _triangulator.ComputedIndexCount);
+            AddInfo(count, _triangulator.ComputedIndexCount, brush);
+
+            int baseVertexIndex = _vertexBufferIndex;
+
+            for (int i = 0; i < count; i++) {
+                AddVertex(points[offset + i], brush);
+            }
+
+            for (int i = 0; i < _triangulator.ComputedIndexCount; i++) {
+                _indexBuffer[_indexBufferIndex + i] = (short)(_triangulator.ComputedIndexes[i] + baseVertexIndex);
+            }
+
+            _indexBufferIndex += _triangulator.ComputedIndexCount;
+        }
+
         private void SetRenderState ()
         {
             _device.BlendState = BlendState.AlphaBlend;
             _device.DepthStencilState = DepthStencilState.None;
             _device.RasterizerState = RasterizerState.CullCounterClockwise;
+            //_device.RasterizerState = new RasterizerState() { FillMode = Microsoft.Xna.Framework.Graphics.FillMode.WireFrame };
             _device.SamplerStates[0] = SamplerState.PointWrap;
 
             _effect.Projection = Matrix.CreateOrthographicOffCenter(0, _device.Viewport.Width, _device.Viewport.Height, 0, -1, 1);
@@ -413,6 +438,114 @@ namespace Amphibian.Drawing
 
             _circleCache.Add(divisions, unitCircle);
             return unitCircle;
+        }
+
+
+        
+    }
+
+    public class Triangulator
+    {
+        private int[] _triPrev = new int[128];
+        private int[] _triNext = new int[128];
+
+        private int[] _indexComputeBuffer = new int[128];
+        private int _indexCount = 0;
+
+        public int[] ComputedIndexes
+        {
+            get { return _indexComputeBuffer; }
+        }
+
+        public int ComputedIndexCount
+        {
+            get { return _indexCount; }
+        }
+
+        public void Triangulate (IList<Vector2> points, int offset, int count)
+        {
+            Initialize(count);
+
+            int index = 0;
+            int computeIndex = 0;
+            while (count >= 3) {
+                bool isEar = true;
+
+                Vector2 a = points[offset + _triPrev[index]];
+                Vector2 b = points[offset + index];
+                Vector2 c = points[offset + _triNext[index]];
+                if (TriangleIsCCW(a, b, c)) {
+                    int k = _triNext[_triNext[index]];
+                    do {
+                        if (PointInTriangleInclusive(points[offset + k], a, b, c)) {
+                            isEar = false;
+                            break;
+                        }
+                        k = _triNext[k];
+                    } while (k != _triPrev[index]);
+                }
+                else {
+                    isEar = false;
+                }
+
+                if (isEar) {
+                    if (_indexComputeBuffer.Length < computeIndex + 3)
+                        Array.Resize(ref _indexComputeBuffer, _indexComputeBuffer.Length * 2);
+
+                    _indexComputeBuffer[computeIndex++] = offset + _triPrev[index];
+                    _indexComputeBuffer[computeIndex++] = offset + index;
+                    _indexComputeBuffer[computeIndex++] = offset + _triNext[index];
+
+                    _triNext[_triPrev[index]] = _triNext[index];
+                    _triPrev[_triNext[index]] = _triPrev[index];
+                    count--;
+                    index = _triPrev[index];
+                }
+                else {
+                    index = _triNext[index];
+                }
+            }
+
+            _indexCount = computeIndex;
+        }
+
+        private void Initialize (int count)
+        {
+            _indexCount = 0;
+
+            if (_triNext.Length < count)
+                Array.Resize(ref _triNext, Math.Max(_triNext.Length * 2, count));
+            if (_triPrev.Length < count)
+                Array.Resize(ref _triPrev, Math.Min(_triPrev.Length * 2, count));
+
+            for (int i = 0; i < count; i++) {
+                _triPrev[i] = i - 1;
+                _triNext[i] = i + 1;
+            }
+
+            _triPrev[0] = count - 1;
+            _triNext[count - 1] = 0;
+        }
+
+        private float Cross2D (Vector2 u, Vector2 v)
+        {
+            return (u.Y * v.X) - (u.X * v.Y);
+        }
+
+        private bool PointInTriangleInclusive (Vector2 point, Vector2 a, Vector2 b, Vector2 c)
+        {
+            if (Cross2D(point - a, b - a) <= 0f)
+                return false;
+            if (Cross2D(point - b, c - b) <= 0f)
+                return false;
+            if (Cross2D(point - c, a - c) <= 0f)
+                return false;
+            return true;
+        }
+
+        private bool TriangleIsCCW (Vector2 a, Vector2 b, Vector2 c)
+        {
+            return Cross2D(b - a, c - b) < 0;
         }
     }
 }
---
+++ Amphibian/Systems/Rendering/Spatials/BackdropSpatial.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Amphibian.EntitySystem;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Amphibian.Components;
+
+namespace Amphibian.Systems.Rendering.Spatials
+{
+    public enum HorizontalClamp
+    {
+        FrameLeft,
+        FrameRight,
+        FrameBoth,
+        WindowLeft,
+        WindowRight,
+        WindowBoth
+    }
+
+    public enum VerticalClamp
+    {
+        FrameTop,
+        FrameBottom,
+        FrameBoth,
+        WindowTop,
+        WindowBottom,
+        WindowBoth
+    }
+
+    public class BackdropSpatial : Spatial
+    {
+        #region Property Data
+
+        internal enum DimClamp
+        {
+            FrameNeg,
+            FramePos,
+            FrameBoth,
+            WindowNeg,
+            WindowPos,
+            WindowBoth
+        }
+
+        #region Clamping Conversions
+
+        private DimClamp HorizontalToDimClamp (HorizontalClamp clamp)
+        {
+            switch (clamp) {
+                case HorizontalClamp.FrameBoth: return DimClamp.FrameBoth;
+                case HorizontalClamp.FrameLeft: return DimClamp.FrameNeg;
+                case HorizontalClamp.FrameRight: return DimClamp.FramePos;
+                case HorizontalClamp.WindowBoth: return DimClamp.WindowBoth;
+                case HorizontalClamp.WindowLeft: return DimClamp.WindowNeg;
+                case HorizontalClamp.WindowRight: return DimClamp.WindowPos;
+                default: return DimClamp.FrameNeg;
+            }
+        }
+
+        private DimClamp VerticalToDimClamp (VerticalClamp clamp)
+        {
+            switch (clamp) {
+                case VerticalClamp.FrameBoth: return DimClamp.FrameBoth;
+                case VerticalClamp.FrameTop: return DimClamp.FrameNeg;
+                case VerticalClamp.FrameBottom: return DimClamp.FramePos;
+                case VerticalClamp.WindowBoth: return DimClamp.WindowBoth;
+                case VerticalClamp.WindowTop: return DimClamp.WindowNeg;
+                case VerticalClamp.WindowBottom: return DimClamp.WindowPos;
+                default: return DimClamp.FrameNeg;
+            }
+        }
+
+        private HorizontalClamp DimToHorizontalClamp (DimClamp clamp)
+        {
+            switch (clamp) {
+                case DimClamp.FrameBoth: return HorizontalClamp.FrameBoth;
+                case DimClamp.FrameNeg: return HorizontalClamp.FrameLeft;
+                case DimClamp.FramePos: return HorizontalClamp.FrameRight;
+                case DimClamp.WindowBoth: return HorizontalClamp.WindowBoth;
+                case DimClamp.WindowNeg: return HorizontalClamp.WindowLeft;
+                case DimClamp.WindowPos: return HorizontalClamp.WindowRight;
+                default: return HorizontalClamp.FrameLeft;
+            }
+        }
+
+        private VerticalClamp DimToVerticalClamp (DimClamp clamp)
+        {
+            switch (clamp) {
+                case DimClamp.FrameBoth: return VerticalClamp.FrameBoth;
+                case DimClamp.FrameNeg: return VerticalClamp.FrameTop;
+                case DimClamp.FramePos: return VerticalClamp.FrameBottom;
+                case DimClamp.WindowBoth: return VerticalClamp.WindowBoth;
+                case DimClamp.WindowNeg: return VerticalClamp.WindowTop;
+                case DimClamp.WindowPos: return VerticalClamp.WindowBottom;
+                default: return VerticalClamp.FrameTop;
+            }
+        }
+
+        #endregion
+
+        private class DimProperties
+        {
+            public int OriginalLength;
+            public float Scale;
+
+            public int Length
+            {
+                get { return (int)(OriginalLength * Scale); }
+            }
+
+            public bool Scroll;
+            public bool Repeat;
+            public bool AutoScroll;
+
+            public float ScrollCoef = 1.0f;
+            public float AutoSpeed = 0.0f;
+            public float AutoPosition = 0.0f;
+
+            public DimClamp Clamp;
+            public int ClampOffset;
+
+            public DimProperties ()
+            {
+                Clamp = DimClamp.FrameNeg;
+                ClampOffset = 0;
+            }
+
+            public int Origin (int winPos, int winLength, int frameLength)
+            {
+                if (Scroll) {
+                    if (Repeat) {
+                        switch (Clamp) {
+                            case DimClamp.FrameNeg:
+                                return winPos - (((int)(winPos * ScrollCoef) + ClampOffset + (int)AutoPosition) % Length);
+                            case DimClamp.FramePos:
+                                // Don't really care if this is correct or not...
+                                return (winPos + winLength) - ((winLength + ((frameLength - (winPos + winLength) - ClampOffset + (int)AutoPosition) % Length) + Length - 1) / Length);
+                            case DimClamp.WindowNeg:
+                                return winPos - (Length - (ClampOffset % Length));
+                            case DimClamp.WindowPos:
+                                return (winPos + winLength) - Length - ClampOffset;
+                            default: return 0;
+                        }
+                    }
+                    else {
+                        switch (Clamp) {
+                            default:
+                                return winPos - (int)((1.0 - ((frameLength - winLength) - winPos) / (float)(frameLength - winLength)) * (float)(Length - winLength));
+                        }
+                    }
+                }
+                else {
+                    switch (Clamp) {
+                        case DimClamp.FrameNeg:
+                        case DimClamp.FrameBoth:
+                            return ClampOffset;
+                        case DimClamp.FramePos:
+                            return frameLength - Length - ClampOffset;
+                        default: return 0;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        private DimProperties _dimX = new DimProperties();
+        private DimProperties _dimY = new DimProperties();
+
+        private Texture2D _spriteTexture;
+
+        public BackdropSpatial (EntityWorld world, ContentManager contentManager, string asset)
+            : base(world)
+        {
+            _spriteTexture = contentManager.Load<Texture2D>(asset);
+            _dimX.OriginalLength = _spriteTexture.Width;
+            _dimY.OriginalLength = _spriteTexture.Height;
+        }
+
+        #region Properties
+
+        public bool ScrollX
+        {
+            get { return _dimX.Scroll; }
+            set { _dimX.Scroll = value; }
+        }
+
+        public bool ScrollY
+        {
+            get { return _dimY.Scroll; }
+            set { _dimY.Scroll = value; }
+        }
+
+        public float ScrollXCoefficient
+        {
+            get { return _dimX.ScrollCoef; }
+            set { _dimX.ScrollCoef = value; }
+        }
+
+        public float ScrollYCoefficient
+        {
+            get { return _dimY.ScrollCoef; }
+            set { _dimY.ScrollCoef = value; }
+        }
+
+        public bool RepeatX
+        {
+            get { return _dimX.Repeat; }
+            set { _dimX.Repeat = value; }
+        }
+
+        public bool RepeatY
+        {
+            get { return _dimY.Repeat; }
+            set { _dimY.Repeat = value; }
+        }
+
+        public bool AutoScrollX
+        {
+            get { return _dimX.AutoScroll; }
+            set { _dimX.AutoScroll = value; }
+        }
+
+        public bool AutoScrollY
+        {
+            get { return _dimY.AutoScroll; }
+            set { _dimY.AutoScroll = value; }
+        }
+
+        public float AutoScrollXSpeed
+        {
+            get { return _dimX.AutoSpeed; }
+            set { _dimX.AutoSpeed = value; }
+        }
+
+        public float AutoScrollYSpeed
+        {
+            get { return _dimY.AutoSpeed; }
+            set { _dimY.AutoSpeed = value; }
+        }
+
+        public float AutoScrollXPosition
+        {
+            get { return _dimX.AutoPosition; }
+            set { _dimX.AutoPosition = value; }
+        }
+
+        public float AutoScrollYPosition
+        {
+            get { return _dimY.AutoPosition; }
+            set { _dimY.AutoPosition = value; }
+        }
+
+        public float ScaleX
+        {
+            get { return _dimX.Scale; }
+            set { _dimX.Scale = value; }
+        }
+
+        public float ScaleY
+        {
+            get { return _dimY.Scale; }
+            set { _dimY.Scale = value; }
+        }
+
+        public HorizontalClamp HorizontalClamp
+        {
+            get { return DimToHorizontalClamp(_dimX.Clamp); }
+            set { _dimX.Clamp = HorizontalToDimClamp(value); }
+        }
+
+        public int HorizontalClampOffset
+        {
+            get { return _dimX.ClampOffset; }
+            set { _dimX.ClampOffset = value; }
+        }
+
+        public VerticalClamp VerticalClamp
+        {
+            get { return DimToVerticalClamp(_dimY.Clamp); }
+            set { _dimY.Clamp = VerticalToDimClamp(value); }
+        }
+
+        public int VerticalClampOffset
+        {
+            get { return _dimY.ClampOffset; }
+            set { _dimY.ClampOffset = value; }
+        }
+
+        #endregion
+
+        public override void Update ()
+        {
+            if (_dimX.AutoScroll) {
+                _dimX.AutoPosition += _dimX.AutoSpeed * (float)World.GameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (_dimY.AutoScroll) {
+                _dimY.AutoPosition += _dimY.AutoSpeed * (float)World.GameTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+
+        public override void Render (SpriteBatch spriteBatch, Entity entity, Renderable position)
+        {
+            if (_spriteTexture == null)
+                return;
+
+            CameraSystem camera = World.SystemManager.GetSystem(typeof(CameraSystem)) as CameraSystem;
+            Rectangle window = (camera != null)
+                ? camera.Bounds : World.Frame.Engine.GraphicsDevice.Viewport.Bounds;
+
+            int originX = _dimX.Origin(window.X, window.Width, World.Frame.Width);
+            int originY = _dimY.Origin(window.Y, window.Height, World.Frame.Height);
+
+            int tileX = 1;
+            int tileY = 1;
+
+            if (_dimX.Repeat) {
+                tileX = (window.X + window.Width - originX + _dimX.Length) / _dimX.Length;
+            }
+            if (_dimY.Repeat) {
+                tileY = (window.Y + window.Height - originY + _dimY.Length) / _dimY.Length;
+            }
+
+            for (int x = 0; x < tileX; x++) {
+                int xStart = originX + (x * _dimX.Length);
+                for (int y = 0; y < tileY; y++) {
+                    int yStart = originY + (y * _dimY.Length);
+                    spriteBatch.Draw(_spriteTexture,
+                        new Rectangle(xStart, yStart, _dimX.Length, _dimY.Length),
+                        new Rectangle(0, 0, _spriteTexture.Width, _spriteTexture.Height),
+                        Color.White);
+                }
+            }
+        }
+    }
+}
