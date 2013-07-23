e812953357281a7fcaedf257c68fd3dd7d318264
Justin Aquadro <jaquadro@gmail.com>
2013-07-22 20:59:41 -0400

    Modify BackgroundCollisionSystem to handle multiple collision managers.

--- Amphibian/Systems/BackgroundCollisionSystem.cs
+++ Amphibian/Systems/BackgroundCollisionSystem.cs
@@ -10,70 +10,101 @@ namespace Amphibian.Systems
 {
     public class BackgroundCollisionSystem : BaseSystem
     {
-        private TileCollisionManager _tileMan;
+        //private TileCollisionManager _tileMan;
+
+        private List<TileCollisionManager> _managers;
 
         public BackgroundCollisionSystem ()
         {
+            _managers = new List<TileCollisionManager>();
         }
 
         public BackgroundCollisionSystem (TileCollisionManager tileMan)
             : this()
         {
-            _tileMan = tileMan;
+            //_tileMan = tileMan;
+            _managers.Add(tileMan);
         }
 
-        public TileCollisionManager TileCollisionManager
+        public void AddCollisionManager (TileCollisionManager manager)
+        {
+            _managers.Add(manager);
+        }
+
+        public void RemoveCollisionManager (TileCollisionManager manager)
+        {
+            _managers.Remove(manager);
+        }
+
+        public void ClearCollisionManagers ()
+        {
+            _managers.Clear();
+        }
+
+        /*public TileCollisionManager TileCollisionManager
         {
             get { return _tileMan; }
             set { _tileMan = value; }
-        }
+        }*/
 
         public bool Test (FPInt x, FPInt y)
         {
-            if (_tileMan == null)
-                return false;
+            foreach (TileCollisionManager manager in _managers) {
+                if (manager.OverlapsAny(x, y))
+                    return true;
+            }
 
-            return _tileMan.OverlapsAny(x, y);
+            return false;
         }
 
         public bool Test (AXLine line)
         {
-            if (_tileMan == null)
-                return false;
+            foreach (TileCollisionManager manager in _managers) {
+                if (manager.OverlapsAny(line))
+                    return true;
+            }
 
-            return _tileMan.OverlapsAny(line);
+            return false;
         }
 
         public bool Test (AYLine line)
         {
-            if (_tileMan == null)
-                return false;
+            foreach (TileCollisionManager manager in _managers) {
+                if (manager.OverlapsAny(line))
+                    return true;
+            }
 
-            return _tileMan.OverlapsAny(line);
+            return false;
         }
 
         public bool TestEdge (FPInt x, FPInt y)
         {
-            if (_tileMan == null)
-                return false;
+            foreach (TileCollisionManager manager in _managers) {
+                if (manager.OverlapsEdgeAny(x, y))
+                    return true;
+            }
 
-            return _tileMan.OverlapsEdgeAny(x, y);
+            return false;
         }
 
         public bool TestEdge (AXLine line)
         {
-            if (_tileMan == null)
-                return false;
+            foreach (TileCollisionManager manager in _managers) {
+                if (manager.OverlapsEdgeAny(line))
+                    return true;
+            }
 
-            return _tileMan.OverlapsEdgeAny(line);
+            return false;
         }
 
         public bool TestEdge (AYLine line)
         {
-            if (_tileMan == null)
-                return false;
+            foreach (TileCollisionManager manager in _managers) {
+                if (manager.OverlapsEdgeAny(line))
+                    return true;
+            }
 
-            return _tileMan.OverlapsEdgeAny(line);
+            return false;
         }
     }
 }
