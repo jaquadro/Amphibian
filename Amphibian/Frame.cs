ef641525c93c5db1ce44830d4023e68dfdf27bec
Justin Aquadro <jaquadro@gmail.com>
2013-11-13 00:47:41 -0500

    Replacing FrameStack implementation; relaxing engine pre-binding constraint

--- Amphibian/Frame.cs
+++ Amphibian/Frame.cs
@@ -35,10 +35,11 @@ namespace Amphibian
         public Frame ()
         { }
 
-        public Frame (Engine engine)
+        internal Frame (Engine engine)
             : this()
         {
-            engine.PushFrame(this);
+            //engine.PushFrame(this);
+            Engine = engine;
         }
 
         #region Properties
