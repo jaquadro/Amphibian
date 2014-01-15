691a09502d33dd19ad057a8df2b617567382abea
Justin Aquadro <jaquadro@gmail.com>
2014-01-15 00:10:48 -0500

    Add DrawCommandSpatial

---
+++ Amphibian/Systems/Rendering/DrawCommandSpatial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Amphibian.Components;
+using Amphibian.EntitySystem;
+using LilyPath;
+
+namespace Amphibian.Systems.Rendering
+{
+    public class DrawCommandSpatial : DrawSpatial
+    {
+        public DrawCommandSpatial ()
+        { }
+
+        public DrawCommandSpatial (Action<DrawBatch, Renderable> command)
+        {
+            Command = command;
+        }
+
+        public Action<DrawBatch, Renderable> Command { get; set; }
+
+        public override void Render (DrawBatch drawBatch, EntityWorld world, Entity entity, Renderable position)
+        {
+            if (Command != null)
+                Command(drawBatch, position);
+        }
+    }
+}
